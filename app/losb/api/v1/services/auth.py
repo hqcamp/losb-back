57a305dd3e072376dde1ca9ef949e46f4ea0513d
HardQode <the@hardqode.com>
2024-11-05 13:25:11 +0300

    Авторизация упрощенная auth.py

--- app/losb/api/v1/services/auth.py
+++ app/losb/api/v1/services/auth.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+import urllib.parse
 import jwt
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
@@ -33,14 +34,24 @@ class ExampleAuthentication(authentication.BaseAuthentication):
             # Assume the header does not contain a JSON web token
             return None
 
-        if len(parts) != 2:
+        if len(parts) != 3:
             raise AuthenticationFailed(
-                _("Authorization header must contain two space-delimited values"),
+                _("Authorization header must contain three space-delimited values"),
                 code="bad_authorization_header",
             )
 
-        return parts[1]
+        return parts[2] # там еще tma есть
 
+    def parse_authorization_header(self, query_string):
+        parsed_query = urllib.parse.parse_qs(query_string)
+    
+        for key, value in parsed_query.items():
+            if len(value) == 1:
+                parsed_query[key] = value[0]
+    
+        return parsed_query
+
+    
     def authenticate(self, request):
         header = request.META.get('HTTP_AUTHORIZATION')
         if not header:
@@ -50,18 +61,26 @@ class ExampleAuthentication(authentication.BaseAuthentication):
         if raw_token is None:
             return None
 
-        try:
-            decoded_token = jwt.decode(
-                raw_token,
-                SECRET_KEY,
-                algorithms=['HS256'],
-            )
-        except InvalidTokenError as ex:
-            raise TokenError(_("Token is invalid or expired")) from ex
+        
+        # try:
+        #     decoded_token = jwt.decode(
+        #         raw_token,
+        #         SECRET_KEY,
+        #         algorithms=['HS256'],
+        #     )
+        # except InvalidTokenError as ex:
+        #     raise TokenError(_("Token is invalid or expired")) from ex
 
         try:
+            decoded_token =  self.parse_authorization_header(raw_token)
+        except InvalidTokenError as ex:
+            raise TokenError(_("Token is invalid or expired")) from ex
+        
+        try:
             User = get_user_model()
-            user = User.objects.get(telegram_id=decoded_token.get('telegram_id'))
+            # user = User.objects.get(telegram_id=decoded_token.get('telegram_id'))
+            user_data = json.loads(decoded_token['user'])
+            user, created = User.objects.get_or_create(telegram_id=user_data['id'])
         except User.DoesNotExist:
             raise AuthenticationFailed('No such user')
 
