aa950a0248cf9b1aa38f6d148f309d00bd6048af
HardQode <the@hardqode.com>
2024-11-05 13:55:49 +0300

    Update auth.py - forgot about import

--- app/losb/api/v1/services/auth.py
+++ app/losb/api/v1/services/auth.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-import urllib.parse
+import urllib.parse, json
 import jwt
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
