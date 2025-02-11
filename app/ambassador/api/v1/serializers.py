b633f963e4bcffaa267cce6f783c10751eb8b172
Rainmakeroffire <rainmakeroffire@gmail.com>
2025-02-11 14:45:25 +0100

    feat: added location validation to VideoSerializer

--- app/ambassador/api/v1/serializers.py
+++ app/ambassador/api/v1/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 from ambassador.models import Video
-from ambassador.api.v1.validators import validate_location_field
+from ambassador.api.v1.validators import validate_location_field, validate_location
 
 
 class VideoSerializer(serializers.ModelSerializer):
@@ -10,6 +11,13 @@ class VideoSerializer(serializers.ModelSerializer):
         model = Video
         fields = '__all__'
         read_only_fields = ['user', 'created_at', 'duration']
+
+    def validate_location(self, value):
+        try:
+            validate_location(value)
+        except ValidationError as e:
+            raise serializers.ValidationError(e)
+        return value
 
 
 class VideoUrlSerializer(serializers.Serializer):
